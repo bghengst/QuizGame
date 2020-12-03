2406523b6e0efdbd131830d596add1c834c36bc3
Grant <beauhengst@gmail.com>
2020-12-02 21:09:05 -0500

    added html and hiding pages for home page quiz page and end page

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
 var questions = [
     {
         title: "What does DOM stand for?:",
-        choices: ["Dirivative Objection Model", "Document Object Model", "Date Of Manufacture", "Depth of Market"]
+        choices: ["Dirivative Objection Model", "Document Object Model", "Date Of Manufacture", "Depth of Market"],
         answer: "Document Object Model"
 
     },
 
     {
-        title: "Which one of these git commands creates a note?:"
-        choices: ["git clone", "git add", "git push", "git commit"]
+        title: "Which one of these git commands creates a note?:",
+        choices: ["git clone", "git add", "git push", "git commit"],
         answer: "git commit"
     },
 
     {
-        title: "Which one of these is NOT used as a container in html?:"
-        choices: ["<p>", "<div>", "<br>", "<html>"]
+        title: "Which one of these is NOT used as a container in html?:",
+        choices: ["<p>", "<div>", "<br>", "<html>"],
         answer: "<br>"
 
-    }
+    },
 
     {
         title: "What is the purpose of 'console.log'?:",
-        choices: ["to display data in a browsers console", "to save your code", "to log your code into a repo", "to add functionality to code"]
+        choices: ["to display data in a browsers console", "to save your code", "to log your code into a repo", "to add functionality to code"],
         answer: "to display data in a browsers console"
 
-    }
+    },
 
     {
-        title: "If an array has 4 items in it, what is the index position of the first item?:"
-        choices: ["0", "1", "3", "4"]
+        title: "If an array has 4 items in it, what is the index position of the first item?:",
+        choices: ["0", "1", "3", "4"],
         answer: "0"
-    }
+    },
 ]
 
