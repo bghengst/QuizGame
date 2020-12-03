6a3824dabc3d8667a755b4f7e126a0a0d445f060
Grant <beauhengst@gmail.com>
2020-12-02 20:08:39 -0500

    added question into script

---
+++ script.js
@@ -0,0 +1,10 @@
+var questions = [
+    {
+        title: "What does DOM stand for?:",
+        choices: ["Dirivative Objection Model", "Document Object Model", "Date Of Manufacture", "Depth of Market"]
+        answer: "Document Object Model"
+
+    },
+
+    {}
+]
